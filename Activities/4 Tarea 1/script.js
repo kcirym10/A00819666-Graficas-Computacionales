ea98533f30c0e937cd2aa60bc23ba006feece413
Alejandro Myrick <kcirym10@me.com>
2020-07-08 16:53:31 -0500

    HW1 started

---
+++ Activities/4 Tarea 1/script.js
@@ -0,0 +1,7 @@
+﻿import { jquery } from '../libs/jquery-3.4.1/jquery/jquery-3.4.1.min.js';
+import { gl, glMatrix, mat4 } from '../libs/gl-matrix/gl-matrix.js';
+
+//Main function
+$(document).ready(() => {
+    
+})
