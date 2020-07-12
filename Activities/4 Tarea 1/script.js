89a5243aee18b115c97761479ce49c0ab66ed330
Alejandro Myrick <kcirym10@me.com>
2020-07-12 18:28:15 -0500

    Tarea 1 - Restarted

--- Activities/4 Tarea 1/script.js
+++ Activities/4 Tarea 1/script.js
@@ -1,28 +1,145 @@
-﻿//import { } from '../libs/jquery-3.4.1/jquery-3.4.1.min.js';
-//import { mat4 } from '../libs/gl-matrix/src/index.js';
+﻿import { Vertex } from "../libs/three.js/three.module";
 
 let mat4 = glMatrix.mat4;
+
+/**
+ * 
+ * @param {HTMLCanvasElement} canvas
+ */
+function initWebGL(canvas) {
+    let ctx = null;
+    let msg = 'Error initializing context';
+    //First check try the context
+    try {
+        ctx = canvas.getContext('experimental-webgl');
+    } catch (e) {
+        alert(msg);
+        throw new Error(msg + e.toString());
+    }
+
+    if (!ctx)
+        console.log(msg);
+
+    return ctx;
+}
+
+/**
+ * 
+ * @param {WebGLRenderingContext} gl
+ */
+function clearCanvas(gl) {
+    gl.clearColor(1, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    alert(gl.getParameter(gl.COLOR_CLEAR_VALUE));
+}
+
+/**
+ * 
+ * @param {WebGLRenderingContext} gl
+ * @param {HTMLCanvasElement} canvas
+ */
+function initViewport(gl, canvas) {
+    clearCanvas(gl);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+/**
+ * 
+ * @param {WebGLRenderingContext} gl
+ * @param {vec3} transformation
+ * @param {vec3} rotation
+ */
+function createPyramid(gl, transformation, rotation) {
+    //Step 1 -> Create the geometry and define indices
+    //Define pyramid geometry
+    let vertices = [
+        //Base ->  [0, 1, 2] -> [2, 1, 3] -> [2, 3, 4]
+        -1.0, -1.0, +0.0, //0 -> Far left
+        -0.5, -1.0, +1.0, //1 -> Near left
+        +0.0, -1.0, -1.0, //2 -> Far point
+        +0.5, -1.0, +1.0, //3 -> Near right
+        +1.0, -1.0, +0.0, //4 -> Far right
+
+        //Front face
+
+        //Right front face
+
+        //Right back face
+
+        //Left back face
+
+        //Left front face
+    ];
+    //Define vertex colors
+    let colors = [
+        [1.0, 0.0, 0.0, 1.0], //Base
+        [0.0, 1.0, 0.0, 1.0], //Front face
+        [0.0, 0.0, 1.0, 1.0], //Front right face
+        [1.0, 1.0, 0.0, 1.0], //Back right face
+        [1.0, 0.0, 1.0, 1.0], //Back left face
+        [0.0, 1.0, 1.0, 1.0]  //Front left face
+    ];
+    let vertexColors = [];
+    //Assign a color to each vertex of the shape, in this case 5 vertices for the base and 3 for each triangle
+
+
+    //Create, bind, pass data, unbind buffers
+    //Vertex buffer
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    //Color buffer
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColors);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    //Index buffer
+    let indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexBuffer), gl.STATIC_DRAW);
+}
+
+//Main function
+//JQuery used to ensure proper processing once page has loaded
+$(document).ready(
+    function () {
+        let canvas = document.getElementById('myCanvas');
+        let gl = null;
+        //Step 1 -> Initialize a WebGL context
+        gl = initWebGL(canvas);
+
+        //Step 2 -> Clear the canvas background and initialize the viewport
+        initViewport(gl, canvas);
+
+        //Step 3 -> Create geometries and buffers
+
+    }
+)
+
+
 let projectionMatrix;
 
 let shaderProgram, shaderVertexPositionAttribute, shaderVertexColorAttribute, shaderProjectionMatrixUniform, shaderModelViewMatrixUniform;
 
-let duration = 5000; //ms
+let duration = 5000; // ms
 
 // Attributes: Input variables used in the vertex shader. Since the vertex shader is called on each vertex, these will be different every time the vertex shader is invoked.
 // Uniforms: Input variables for both the vertex and fragment shaders. These do not change values from vertex to vertex.
 // Varyings: Used for passing data from the vertex shader to the fragment shader. Represent information for which the shader can output different value for each vertex.
 let vertexShaderSource =
-    "attribute vec3 vertexPos;\n" +
-    "attribute vec4 vertexColor;\n" +
-    "uniform mat4 modelViewMatrix;\n" +
-    "uniform mat4 projectionMatrix;\n" +
-    "varying vec4 vColor;\n" +
-    "void main(void){\n" +
-    "   // Return the transformed and projected vertex value\n" +
-    "   gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPos, 1.0);\n" +
-    "   //Output the vertexColor into vColor\n" +
-    "   vColor = vertexColor;\n" +
-    "}\n";
+    "    attribute vec3 vertexPos;\n" +
+    "    attribute vec4 vertexColor;\n" +
+    "    uniform mat4 modelViewMatrix;\n" +
+    "    uniform mat4 projectionMatrix;\n" +
+    "    varying vec4 vColor;\n" +
+    "    void main(void) {\n" +
+    "		// Return the transformed and projected vertex value\n" +
+    "        gl_Position = projectionMatrix * modelViewMatrix * \n" +
+    "            vec4(vertexPos, 1.0);\n" +
+    "        // Output the vertexColor in vColor\n" +
+    "        vColor = vertexColor;\n" +
+    "    }\n";
 
 // precision lowp float
 // This determines how much precision the GPU uses when calculating floats. The use of highp depends on the system.
@@ -35,162 +152,6 @@ let fragmentShaderSource =
     "    void main(void) {\n" +
     "    gl_FragColor = vColor;\n" +
     "}\n";
-
-//Initialize WebGL
-//Step 2 -> Obtain context
-function initWebGL(canvas) {
-    let gl = null;
-    let msg = "Your browser does not support WebGL or it is not enabled by default";
-
-    try {
-        gl = canvas.getContext("experimental-webgl");
-    }
-    catch (error) {
-        msg = "Error creating WebGL context: " + error.toString();
-    }
-
-    if (!gl) {
-        alert(msg);
-        throw new Error(msg);
-    }
-
-    return gl;
-}
-
-//Step 3 -> Initialize viewport
-/**
- * 
- * @param {WebGLRenderingContext} gl
- * @param {HTMLCanvasElement} canvas
- */
-function initViewport(gl, canvas) {
-    gl.viewport(0, 0, canvas.width, canvas.height);
-}
-
-//Step 4 -> Create buffers
-/**
- * 
- * @param {HTMLCanvasElement} canvas
- */
-function initGL(canvas) {
-    //Projection matrix with a 45 degree field of view
-    //This is the camera
-    projectionMatrix = mat4.create();
-
-    mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 1, 100);
-    mat4.translate(projectionMatrix, projectionMatrix, [0, 0, -5]);
-}
-
-//Step 5 -> Create transformation matrix
-/**
- * 
- * @param {WebGLRenderingContext} gl
- * @param {number} translation
- * @param {number} rotationAxis
- */
-function createPyramid(gl, translation, rotationAxis) {
-    //Vertex data
-    let vertexBuffer;
-    vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-
-    //Pentagonal pyramid
-    let vertices = [
-        //Bottom face -> pentagon
-        +0.0, -0.5, -0.5,   //0 -> Far point
-        -0.7, -0.5, +0.0,   //1 -> Left mid
-        -0.5, -0.5, +0.5,   //2 -> Bottom left
-        +0.5, -0.5, +0.5,   //3 -> Bottom right
-        +0.7, -0.5, +0.0,   //4 -> Right mid
-
-        //Front face
-        -0.5, -0.5, +0.5,   //5 (2) -> Bottom left
-        +0.5, -0.5, +0.5,   //6 (3) -> Bottom right
-        +0.0, +0.5, +0.0,   //7 -> Top
-
-        //Right front face
-        -0.5, -0.5, +0.5,   //8 (2) -> Bottom left
-        +0.5, -0.5, +0.5,   //9 (3) -> Bottom right
-        +0.0, +0.5, +0.0,   //10 (7) -> Top
-
-        //Right back face
-        +0.7, -0.5, +0.0,   //11 (4) -> Right mid
-        +0.0, -0.5, -0.5,   //12 (0) -> Far point
-        +0.0, +0.5, +0.0,   //13 (7) -> Top
-
-        //Left back face
-        +0.0, -0.5, -0.5,   //14 (0) -> Far point
-        -0.7, -0.5, +0.0,   //15 (1) -> Left mid
-        +0.0, +0.5, +0.0,   //16 (7) -> Top
-
-        //Left front face
-        -0.7, -0.5, +0.0,   //17 (1) -> Left mid
-        -0.5, -0.5, +0.5,   //18 (2) -> Bottom left
-        +0.0, +0.5, +0.0,   //19 (7) -> Top
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    //Color data
-    let colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    let faceColors = [
-        [1.0, 0.0, 0.0, 1.0], //Base color
-        [0.0, 1.0, 0.0, 1.0], //Front face
-        [0.0, 0.0, 1.0, 1.0], //Right front face
-        [1.0, 1.0, 0.0, 1.0], //Right back face
-        [1.0, 0.0, 1.0, 1.0], //Left front face
-        [0.0, 1.0, 1.0, 1.0]  //Left back face  
-    ];
-
-    //Assign a color to each vertex
-    let vertexColors = [];
-    faceColors.forEach(color => {
-        for (let i = 0; i < 3; i++) {
-            vertexColors.push(...color);
-        }
-    });
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
-
-    let pyramidIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndexBuffer);
-
-    //Index data -> defines which vertices form each triangle face
-    let pyramidIndices = [
-        0, 1, 2,    //Base
-        2, 0, 3,    //Base
-        3, 4, 0,    //Base
-        /////////////////////////
-        5, 6, 7,    //Front face
-        8, 9, 10,   //Right front face
-        11, 12, 13, //Right back face
-        14, 15, 16, //Left back face
-        17, 18, 19  //Left front face
-    ];
-    console.log(vertices.length);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidIndices), gl.STATIC_DRAW);
-
-    let pyramid = {
-        buffer: vertexBuffer, colorBuffer: colorBuffer, indices: pyramidIndexBuffer,
-        vertSize: 3, nVerts: vertices.length, colorSize: 3, nColors: vertexColors.length, nInices: pyramidIndices.length,
-        primtype: gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime: Date.now()
-    };
-
-    mat4.translate(pyramid.modelViewMatrix, pyramid.modelViewMatrix, translation);
-
-    pyramid.update = function () {
-        let now = Date.now();
-        let deltaT = now - this.currentTime;
-        this.currentTime = now;
-        let fract = deltaT / duration;
-        let angle = Math.PI * 2 * fract;
-
-        mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
-    };
-
-    return pyramid;
-}
 
 function createShader(gl, str, type) {
     let shader;
@@ -282,31 +243,3 @@ function run(gl, objs) {
     for (i = 0; i < objs.length; i++)
         objs[i].update();
 }
-
-//Main function
-$(document).ready(function () {
-    //Step 1 -> Create canvas
-    /** @type {HTMLCanvasElement} */
-    //Defines canvas as a canvas HTML5 element
-    let canvas = document.getElementById('WebGLCanvas');
-    canvas.style.width = "100%";
-    canvas.style.height = "50%";
-    canvas.style.border = "1px solid black";
-
-    //Step 2 -> Get context
-    //Enables code completion for WebGL in a local context
-    /** @type {WebGLRenderingContext} */
-    let gl = initWebGL(canvas);
-
-    //Step 3 -> Initialize viewport
-    initViewport(gl, canvas);
-
-    //Step 4 -> Create buffers
-    initGL(canvas);
-
-    //Step 5 -> Create transformation matrixes
-    let pyramid = createPyramid(gl, [0, 0, -10], [0, 0, 0]);
-
-    initShader(gl);
-    run(gl, pyramid)
-})
