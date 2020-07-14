1c8781061989837696f121b89236c614f3b711a4
Alejandro Myrick <kcirym10@me.com>
2020-07-13 19:17:17 -0500

    Actividad 0 - Triangulo de sierpinski
    Changed sierpinski`s algorithm and triangle class. Now working correctly and completely finished

--- Activities/0 TrianguloSerpinski/script.js
+++ Activities/0 TrianguloSerpinski/script.js
@@ -1,10 +1,15 @@
 class Triangle {
-    constructor(width, height, xStart, yStart) {
-        this.width = width;
-        this.height = height;
-        this.center = [width / 2, height / 2];
-        this.xStart = xStart;
-        this.yStart = yStart;
+    /**
+     * 
+     * Vertices
+     * @param {vec2} v0 //Top
+     * @param {vec2} v1 //Left
+     * @param {vec2} v2 //Right
+     */
+    constructor(v0, v1, v2) {
+        this.v0 = v0;
+        this.v1 = v1;
+        this.v2 = v2
     }
 }
 
@@ -12,29 +17,53 @@ function clear(context, color, width, height) {
     context.fillStyle = color;
     context.fillRect(0, 0, width, height);
 }
-
+/**
+ * 
+ * @param {CanvasRenderingContext2D} context
+ * @param {any} color
+ * @param {Triangle} t
+ */
 function drawTriangle(context, color, t) {
     context.beginPath();
     context.fillStyle = color;
-    context.moveTo(t.xStart + t.center[0], t.yStart);
-    context.lineTo(t.xStart, t.yStart + t.height);
-    context.lineTo(t.xStart + t.width, t.yStart + t.height);
-    context.lineTo(t.xStart + t.center[0], t.yStart);
-    //context.fill();
-    context.stroke();
+    context.moveTo(t.v0[0], t.v0[1]);
+    context.lineTo(t.v1[0], t.v1[1]);
+    context.lineTo(t.v2[0], t.v2[1]);
+    context.lineTo(t.v0[0], t.v0[1]);
+    context.fill();
     context.closePath();
 }
 
-function drawFractal(context, parentT, childT, subDivision) {
+function sierpinski(context, parentT, subDivision) {
+    console.log(subDivision);
+    console.log(parentT);
     if (subDivision == 0)
-        return drawTriangle(context, "#023E8A", childT);
+        drawTriangle(context, "#023E8A", parentT);
+    else {
+        //Top triangle
+        let child = new Triangle(
+            parentT.v0,
+            [(parentT.v0[0] + parentT.v1[0]) / 2, (parentT.v0[1] + parentT.v1[1]) / 2],
+            [(parentT.v0[0] + parentT.v2[0]) / 2, (parentT.v0[1] + parentT.v2[1]) / 2]
+        );
+        sierpinski(context, child, subDivision - 1);
 
-    //Top triangle                                                                V <----parentT.center[0] ERROR
-    drawFractal(context, childT, new Triangle(childT.center[0], childT.center[1],  childT.topX(), childT.topY()), subDivision - 1);
-    //Left triangle
-    drawFractal(context, childT, new Triangle(childT.center[0], childT.center[1], childT.leftX(), childT.leftY()), subDivision - 1);
-    //Right triangle
-    drawFractal(context, childT, new Triangle(childT.center[0], childT.center[1], childT.rightX(), childT.rightY()), subDivision - 1);
+        //Left triangle
+        child = new Triangle(
+            [(parentT.v0[0] + parentT.v1[0]) / 2, (parentT.v0[1] + parentT.v1[1]) / 2],
+            parentT.v1,
+            [(parentT.v1[0] + parentT.v2[0]) / 2, (parentT.v1[1] + parentT.v2[1]) / 2]
+        );
+        sierpinski(context, child, subDivision - 1);
+
+        //Right triangle
+        child = new Triangle(
+            [(parentT.v0[0] + parentT.v2[0]) / 2, (parentT.v0[1] + parentT.v2[1]) / 2],
+            [(parentT.v1[0] + parentT.v2[0]) / 2, (parentT.v1[1] + parentT.v2[1]) / 2],
+            parentT.v2
+        );
+        sierpinski(context, child, subDivision - 1);
+    }
 }
 
 //Called from onLoad() event
@@ -42,27 +71,28 @@ function main() {
     let width = window.innerWidth / 2;
     //Set correct height. If width is larger than window height then set to 80% window height
     let height = (width <= window.innerHeight ? width : 0.85 * window.innerHeight);
-    console.log(height);
+
     //Get Canvas element and set appriopritate context
     let myCanvas = document.getElementById("myCanvas");
     let context = myCanvas.getContext("2d");
-    let triangle = new Triangle(width, height, 0, 0);
+    let triangle = new Triangle([width / 2,0], [0, height], [width, height]);
 
     //Set canvas size
     myCanvas.width = width;
     myCanvas.height = height;
 
     //Draw undivided triangle
-    drawFractal(context, null, triangle, 0);
+    sierpinski(context, triangle, 0);
     sliderChange(context, myCanvas, triangle);
 }
 
 function sliderChange(context, canvas, baseTriangle) {
     document.getElementById("myRange").oninput = (event) => {
         let rangeValue = event.target.value;
-        console.log(rangeValue);
+
+        console.log(baseTriangle);
         document.getElementById('rangeValue').innerHTML = ` ${rangeValue}`
         clear(context, '#FFFF', canvas.width, canvas.height);
-        drawFractal(context, null, baseTriangle, rangeValue);
+        sierpinski(context, baseTriangle, rangeValue);
     }
 }
