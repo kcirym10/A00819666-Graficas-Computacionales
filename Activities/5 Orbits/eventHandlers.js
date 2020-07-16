e4cbb20e945286dc6ec3f997dbba7417ae387694
Alejandro Myrick <kcirym10@me.com>
2020-07-15 19:26:28 -0500

    Started on Activity 5
    Also created a jsconfig.json file for proper dependenices

---
+++ Activities/5 Orbits/eventHandlers.js
@@ -0,0 +1,80 @@
+﻿///<reference path="../libs/three.js/three.module.js" />
+import { Object3D } from '../libs/three.js/three.module.js';
+/*
+    Needed handlers
+    Range change
+    Mouse down
+    Mouse up
+    Mouse move
+ */
+
+/** 
+ *  @type {boolean} 
+ *  @type {number}
+ */
+let isMouseDown = false, pageX = 0;
+
+/**
+ * 
+ * @param {number} deltaX
+ * @param {Object3D} group
+ */
+function rotateScene(deltaX, group) {
+    
+}
+
+/**
+ * 
+ * @param {number} scale
+ * @param {Object3D} group
+ */
+function scaleScene(scale, group) {
+
+}
+
+/**
+ * 
+ * @param {MouseEvent} event
+ * @param {Object3D} group
+ */
+function onMouseMove(event, group) {
+    if (!isMouseDown)
+        return;
+
+    event.preventDefault();
+    let deltaX = event.pageX - pageX;
+    pageX = event.pageX;
+    
+}
+
+/**
+ * 
+ * @param {MouseEvent} event
+ */
+function onMouseDown(event) {
+    event.preventDefault();
+
+    isMouseDown = true;
+    pageX = event.pageX;
+}
+
+/**
+ * 
+ * @param {MouseEvent} event
+ */
+function onMouseUp(event) {
+    event.preventDefault();
+
+    isMouseDown = false;
+}
+
+/**
+ * 
+ * @param {HTMLCanvasElement} canvas
+ * @param {Object3D} group
+ */
+export function AddEventHandlers(canvas, group) {
+    canvas.addEventListener('mousemove', event => onMouseMove(event, group), false);
+    canvas.addEventListener('mouseup', event => onMouseUp(event), false);
+    canvas.addEventListener('mousedown', event => onMouseDown(event), false);
+}
---
+++ Activities/5 Orbits/script.js
@@ -0,0 +1,39 @@
+﻿import * as three from '../libs/three.js/three.js';
+import { AddEventHandlers } from './eventHandlers.js';
+
+/**
+ * 
+ * @param {HTMLCanvasElement} canvas
+ */
+function initCanvas(canvas) {
+    canvas = document.getElementById('myCanvas');
+
+    if (!canvas) {
+        console.log('No canvas found');
+        return;
+    }
+    alert('success');
+}
+
+/**
+ * 
+ * @param {three.Renderer} renderer
+ * @param {three.Scene} scene
+ * @param {three.PerspectiveCamera} camera
+ */
+function initScene(renderer, scene, camera) {
+
+}
+
+$(document).ready(
+    function () {
+        let canvas = null,
+            renderer = null,
+            scene = null,
+            camera = null;
+
+        initCanvas(canvas);
+
+        AddEventHandlers(canvas, group);
+    }
+);
